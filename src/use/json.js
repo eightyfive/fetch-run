6e70c9f1a8c862f06e6cd00f1cc22910b2d510be
Benoit Sagols <benoit.sagols@gmail.com>
2018-11-07 11:33:44 +0700

    Better Json response middleware

--- src/use/json.js
+++ src/use/json.js
@@ -1,8 +1,12 @@
+const regExp = /([\/+])json$/;
+
 export default function jsonResponse(next) {
   return async req => {
     const res = await next(req);
+    const contentType = res.headers.get("Content-Type");
+    const isJson = regExp.test(contentType);
 
-    if (res.status >= 200 && res.status < 300) {
+    if (res.status >= 200 && res.status < 300 && isJson) {
       return await res.json();
     }
 
