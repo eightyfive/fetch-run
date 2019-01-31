6b69811952fbf7f44979d2b00e45e7d7c56f61e2
Benoit Sagols <benoit.sagols@gmail.com>
2019-01-31 11:40:30 +0700

    Default HTTP Error message

--- src/http-error.js
+++ src/http-error.js
@@ -1,6 +1,6 @@
 export default class HttpError extends Error {
   constructor(res, data) {
-    super(res.statusText);
+    super(res.statusText || `HTTP Error ${res.status}`);
 
     this.response = res;
     this.data = data;
