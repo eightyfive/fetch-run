1c41c3dfbd72980c23a757bb25902d5c2281f5e8
Benoit Sagols <benoit.sagols@gmail.com>
2019-02-05 09:49:34 +0700

    Better set headers (inherit)

--- src/use/headers.js
+++ src/use/headers.js
@@ -1,31 +1,41 @@
-import parseUrl from "../parse-url";
+import parseUrl from '../parse-url';
 
 export default function createSetHeaders(headers) {
   return next => async req => {
     const url = parseUrl(req.url);
-    const prefix = headers._prefix ? `/${headers._prefix}/` : "/";
-    const pathname = url.pathname.replace(prefix, "");
+    const prefix = headers._prefix ? `/${headers._prefix}/` : '/';
+    const pathname = url.pathname.replace(prefix, '');
 
-    for (let pattern in headers) {
-      let re;
-
-      if (pattern.indexOf("_") === 0) {
+    for (let method in headers) {
+      if (method.indexOf('_') === 0) {
         continue;
       }
 
-      if (pattern === "*") {
-        re = new RegExp("^.*$");
-      } else {
-        re = new RegExp(pattern);
-      }
+      if (method === '*') {
+        setHeaders(req, headers[method]);
+      } else if (method === req.method.toLowerCase()) {
+        for (let pattern in headers[method]) {
+          const re = new RegExp(pattern);
 
-      if (re.test(pathname)) {
-        for (let name in headers[pattern]) {
-          req.headers.set(name, headers[pattern][name]);
+          if (re.test(pathname)) {
+            setHeaders(req, headers[method][pattern]);
+          }
         }
       }
     }
 
     return next(req);
   };
+}
+
+function setHeaders(req, headers) {
+  for (let key in headers) {
+    const val = headers[key];
+
+    if (val === false) {
+      req.headers.delete(key);
+    } else {
+      req.headers.set(key, val);
+    }
+  }
 }
