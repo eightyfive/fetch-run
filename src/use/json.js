0f8370a712d73e774b5f30448d40bde3c01627e3
Benoit Sagols <benoit.sagols@gmail.com>
2019-04-19 23:12:32 +0700

    Dont check if response ok

--- src/use/json.js
+++ src/use/json.js
@@ -1,14 +1,14 @@
 const isJson = /([\/+])json$/;
 
 export default function jsonResponse(next) {
-  return async req => {
-    const res = await next(req);
-    const contentType = res.headers.get('Content-Type');
+	return async req => {
+		const res = await next(req);
+		const contentType = res.headers.get('Content-Type');
 
-    if (isJson.test(contentType) && res.status >= 200 && res.status < 300) {
-      return await res.json();
-    }
+		if (isJson.test(contentType)) {
+			return await res.json();
+		}
 
-    return res;
-  };
+		return res;
+	};
 }
