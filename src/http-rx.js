08d36cd5ea744ce7a36f3195045d851c160a2f69
Benoit Sagols <benoit.sagols@gmail.com>
2019-12-31 09:23:33 +0800

    Rx kernel middleware

--- src/http-rx.js
+++ src/http-rx.js
@@ -1,10 +1,12 @@
 import { of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { fromFetch } from 'rxjs/fetch';
 
 import HttpBase from './http-base';
 
 export default class HttpRx extends HttpBase {
-  getKernel(req) {
-    // TODO
+  getKernel(req$) {
+    return next => req$ => req$.pipe(switchMap(req => fromFetch(req)));
   }
 
   run(req) {
