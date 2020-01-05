708e047d579a8ac75c045323d7924b27aa4123b2
Benoit Sagols <benoit.sagols@gmail.com>
2020-01-05 16:06:22 +0700

    Add test to how-to log req in after middleware (res)

--- src/http-rx.test.js
+++ src/http-rx.test.js
@@ -1,5 +1,5 @@
 import { from } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 
 import Http from './http-rx';
 
@@ -54,6 +54,32 @@ describe('HttpRx', () => {
     api.get('api/resource').subscribe(data => {
       expect(data).toEqual({ foo: 'bar' });
 
+      done();
+    });
+  });
+
+  it('logs req in after middleware (res)', done => {
+    const log = jest.fn();
+
+    api.use(next => oldReq$ => {
+      const newReq$ = oldReq$.pipe(
+        map(oldReq => new Request(oldReq, { method: 'POST' })),
+      );
+
+      let _req;
+
+      newReq$.subscribe(req => {
+        _req = req;
+      });
+
+      return next(newReq$).pipe(tap(res => log(_req)));
+    });
+
+    api.get('api/resource').subscribe(res => {
+      const req = fetch.mock.calls[0][0];
+
+      expect(req.method).toBe('POST');
+      expect(log).toHaveBeenLastCalledWith(req);
       done();
     });
   });
