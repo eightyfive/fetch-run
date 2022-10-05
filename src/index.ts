4079fc7208247eabc9436855cb3ace90e49a0b40
0x55 <0xfiftyfive@gmail.com>
2022-10-05 17:20:01 +0700

    Remove uses from root export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import { error, logger, xsrf } from './use';
-
 export * from './api';
 export * from './http';
 export * from './error';
 export * from './types';
-
-export const uses = { error, logger, xsrf };
