bcf89b1bd79f1ab706f97d10246260228f64ff10
0x55 <0xfiftyfive@gmail.com>
2022-10-06 17:40:50 +0700

    Remove comment

--- src/use/error-metro.ts
+++ src/use/error-metro.ts
@@ -13,14 +13,6 @@ export const errorMetro: Middleware =
       return response;
     }
 
-    // When throwing a custom `Error`:
-
-    // throw new HTTPError(res, req);
-
-    // Metro bundler somehow tries to fetch <app-root>/HTTPError@...
-    // Resulting in `Error: ENOENT: no such file or directory` in console.
-
-    // This is the workaround (throw a normal `Error`)
     const err = new Error(
       res.statusText ? `${res.statusText} (${res.status})` : `${res.status}`,
     );
