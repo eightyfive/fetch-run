eb6acff696439133d1a3fc41663b907005cc288c
0x55 <0xfiftyfive@gmail.com>
2022-10-06 10:05:24 +0700

    Emulate `HTTPError` (throw normal `Error`)

--- src/use/error.ts
+++ src/use/error.ts
@@ -1,12 +1,33 @@
 import { HTTPError } from '../error';
 import { Layer, Middleware } from '../types';
 
-export const error: Middleware = (next: Layer) => async (req: Request) => {
-  const res = await next(req);
+export const error: Middleware = (next: Layer) => async (request: Request) => {
+  const req = request.clone();
 
-  if (res.ok) {
-    return res;
+  const response = await next(request);
+
+  const res = response.clone();
+
+  if (response.ok) {
+    return response;
   }
 
-  throw new HTTPError(res, req);
+  // When throwing a custom `Error`:
+
+  // throw new HTTPError(res, req);
+
+  // Metro bundler somehow tries to fetch <app-root>/HTTPError@...
+  // Resulting in `Error: ENOENT: no such file or directory` in console.
+
+  // This is the workaround (throw a normal `Error`)
+  const err = new Error(
+    res.statusText ? `${res.statusText} (${res.status})` : `${res.status}`,
+  );
+
+  (err as HTTPError).name = 'HTTPError';
+  (err as HTTPError).code = res.status;
+  (err as HTTPError).request = req;
+  (err as HTTPError).response = res;
+
+  throw err as HTTPError;
 };
