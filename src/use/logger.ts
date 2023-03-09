dc66ac92f90347b07ea5da36d4909cba6b53a01e
0x55 <0xfiftyfive@gmail.com>
2023-03-09 16:39:33 +0700

    Add verbose Logger option

--- src/use/logger.ts
+++ src/use/logger.ts
@@ -2,43 +2,51 @@ import { Layer, Middleware } from '../types';
 
 const RE_HOSTNAME = /^https?:\/\/[\w-\.:]+/;
 
-export const logger: Middleware = (next: Layer) => async (request: Request) => {
-  const req = request.clone();
-
-  const response = await next(request);
-
-  const res = response.clone();
-
-  const pathname = req.url.replace(RE_HOSTNAME, '');
-
-  let contents = `${req.method} ${pathname || '/'} (${res.status})`;
-
-  if (res.headers.get('Content-Type') === 'application/json') {
-    // Log request data
-    try {
-      contents += '\n';
-      contents += prettify(await req.json());
-    } catch (err) {
-      // Ignore
-    }
-
-    const data = await res.json();
-
-    // Log response data
-    if (data) {
-      contents += '\n';
-      contents += prettify(data);
-    }
-  }
-
-  if (res.status >= 300) {
-    console.error(contents);
-  } else {
-    console.log(contents);
-  }
-
-  return response;
+type LoggerOptions = {
+  verbose: boolean;
 };
+
+export function createLogger({ verbose }: LoggerOptions) {
+  return (next: Layer) => async (request: Request) => {
+    const req = request.clone();
+
+    const response = await next(request);
+
+    const res = response.clone();
+
+    const pathname = req.url.replace(RE_HOSTNAME, '');
+
+    let contents = `${req.method} ${pathname ?? '/'} (${res.status})`;
+
+    if (verbose && res.headers.get('Content-Type') === 'application/json') {
+      // Log request data
+      try {
+        contents += '\n';
+        contents += prettify(await req.json());
+      } catch (err) {
+        // Ignore
+      }
+
+      const data = await res.json();
+
+      // Log response data
+      if (data) {
+        contents += '\n';
+        contents += prettify(data);
+      }
+    }
+
+    if (res.status >= 300) {
+      console.error(contents);
+    } else {
+      console.log(contents);
+    }
+
+    return response;
+  };
+}
+
+export const logger: Middleware = createLogger({ verbose: true });
 
 function prettify(data: any) {
   return JSON.stringify(data, null, 2);
