2d9dab6201bea2bcaba1f0b081fa9751d08e4957
0x55 <0xfiftyfive@gmail.com>
2026-09-01 23:02:58 +0200

    feat: expose response and data to subscribers

--- src/utils.ts
+++ src/utils.ts
@@ -10,28 +10,15 @@ export function toJSON<T>(res: Response) {
   return data as Promise<T>;
 }
 
-type Json = string | number | boolean | Json[] | { [key: string]: Json };
+export type Json =
+  | string
+  | number
+  | boolean
+  | Json[]
+  | { [key: string]: Json };
 
-export type ResponseParsed = {
-  data: Json | null;
-  headers: Headers;
-  ok: boolean;
-  status: number;
-  statusText: string;
-  type: ResponseType;
-  url: string;
-};
-
-export async function parseResponse(res: Response): Promise<ResponseParsed> {
+export async function parseResponseData(res: Response): Promise<Json | null> {
   const data: Json | null = await res.json().catch(() => null);
 
-  return {
-    data,
-    headers: res.headers,
-    ok: res.ok,
-    status: res.status,
-    statusText: res.statusText,
-    type: res.type,
-    url: res.url,
-  };
+  return data;
 }
